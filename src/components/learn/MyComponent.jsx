e681933afd26b63fde0cccb3d6b20a89aa3b344f
Hanh73 <ndhthtr@gmail.com>
2024-09-07 19:13:33 +0700

    Session 5

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,11 +1,19 @@
 import './style.css';
 
 const MyComponent = () => {
+    // const Hanh1 = 'Hanh1';
+    const Hanh1 = {
+        Name: "Hanh1",
+        Age: 25
+    }
     return (
         <>
-            <div> Hanh study React updated </div>
+            <div> {JSON.stringify(Hanh1)} & study React updated </div>
+            <div>{console.log("Hanh Check")}</div>
             <div className="child"
-                style={{ borderRadius: "10px" }}
+                style={
+                    { borderRadius: "10px" }
+                }
             >child 1</div>
         </>
 
