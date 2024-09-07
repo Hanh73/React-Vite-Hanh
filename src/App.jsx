c10b15a361fd2ef8dc556bdf9264d47d59401cb7
Hanh73 <ndhthtr@gmail.com>
2024-09-07 13:39:15 +0700

    Session 1

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,13 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  // function myFunction() {
+  //   console.log(">>> Run my function")
+  // }
+  const myFunction = () => {
+    console.log(">>> Run my function")
+  }
+  myFunction();
   return (
     <>
       <div>
@@ -16,7 +22,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Hello Gama</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
